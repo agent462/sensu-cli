e87584e8edfae976c7d18ec4ba8cc5f0953dfe31
bryan.brandau <agent462@gmail.com>
2013-03-30 13:31:28 -0500

    puts config file in ~.sensu

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,19 +1,30 @@
 require 'json'
+require 'fileutils'
 
 module SensuCli
   class Settings
 
     def self.load_file
+      directory = "#{Dir.home}/.sensu"
+      file = "#{directory}/settings.json"
       settings = {}
-      file = '../settings.json'
       if File.readable?(file)
         begin
           settings = JSON.parse(File.open(file, 'r').read, :symbolize_names => true)
         rescue JSON::ParserError => e
-          puts "The config file must be valid json. #{e}"
+          puts "The config file must be valid json. #{e}".color(:red)
+          exit
         end
       else
-        puts "The config file does not exist or is not readable. #{e}"
+        if File.directory?(directory)
+          FileUtils.cp("../settings.example.json", file)
+          puts "We created the configuration file for you at #{file}.  Edit the settings as needed.".color(:red)
+        else
+          FileUtils.mkdir(directory)
+          FileUtils.cp("../settings.example.json", file)
+          puts "The settings file and directory did not exist. We created it for you #{file}. Edit the settings as needed.".color(:red)
+        end
+        exit
       end
     end
 
