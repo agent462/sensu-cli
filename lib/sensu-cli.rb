46074a4170641c618cc34a0ab2f61dca6ae019fd
Bryan Brandau <agent462@gmail.com>
2013-06-01 21:30:29 -0500

    move the requires to sensu-cli for easy inclusion

--- lib/sensu-cli.rb
+++ lib/sensu-cli.rb
@@ -1,2 +1,11 @@
+require 'sensu-cli/settings'
+require 'sensu-cli/cli'
+require 'sensu-cli/editor'
+require 'sensu-cli/pretty'
+require 'sensu-cli/path.rb'
+require 'sensu-cli/api.rb'
+require 'sensu-cli/base.rb'
+require 'sensu-cli/version.rb'
+
 module SensuCli
 end
