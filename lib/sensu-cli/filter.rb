91e0caf8cdf983a8851e1ce50d72fad6a82e0b02
Bryan Brandau <agent462@gmail.com>
2014-05-05 21:28:20 -0700

    minor filter cleanup

--- lib/sensu-cli/filter.rb
+++ lib/sensu-cli/filter.rb
@@ -8,10 +8,10 @@ module SensuCli
     def self.filter_json(res, filter)
       filter = self.filter_split(filter)
       if !res.empty?
-        response = res.select do |item|
+        res.select do |item|
           if item.key? filter[0]
             if item[filter[0]].is_a?(Array)
-              item unless item[filter[0]].select { |item| item.include? filter[1] }.empty?
+              item unless item[filter[0]].select { |value| value.include? filter[1] }.empty?
             else
               item if item[filter[0]].include? filter[1]
             end
